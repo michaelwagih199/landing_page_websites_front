72f833f2712a2ded2e35af8d582877298c291ab9
nada-aliiiiiiiiiiiiiii <nada63958@gmail.com>
2024-12-28 00:29:56 +0200

    our-team-section-responsive

--- src/app/website/widgets/team-card.component.ts
+++ src/app/website/widgets/team-card.component.ts
@@ -10,9 +10,9 @@ import { ImageShadowEffectWidget } from "./image-effect.component";
   <image_shadow_effect_widget imageSrc="{{ cardCover }}" imageClass="relative group h-[20rem] w-[17.813rem]"
     class="transition duration-300 ease-in-out rounded-lg">
   </image_shadow_effect_widget>
-  <div class="text-darkGrey flex flex-col gap-2 mt-2">
-    <p class="font-medium text-2xl ">{{ cardTitle }}</p>
-    <p>{{ cardDesc }}</p>
+  <div class="text-darkGrey flex flex-col sm:gap-2 gap-0 mt-2 text-center sm:text-left">
+    <p class="font-medium sm:text-2xl text-xl">{{ cardTitle }}</p>
+    <p class="text-sm sm:text-base">{{ cardDesc }}</p>
   </div>
 </div>
   `,
